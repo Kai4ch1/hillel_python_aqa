d880a88db75f5771039e6d3544357447e31d0370
Kai4ch1 <carl4welps@gmail.com>
2025-09-27 04:35:21 +0300

    Homework 6.1 added

---
+++ lesson_6/homework_6_01.py
@@ -0,0 +1,18 @@
+
+def task_1():
+    print("Hello! Enter your text down below:")
+    user_input = input()
+
+    altered_user_input = set(user_input)
+    print(f"- Amount of unique chars is: {altered_user_input}\n- Amount of the unique symbols is: {len(altered_user_input)}")
+    print("-"*200)
+
+    if len(altered_user_input) > 10:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    result = task_1()
+    print(f"\n The final Result is: {result}")
+
