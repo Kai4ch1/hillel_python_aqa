f672e371d8959f9055c1892db35e68416f595a08
Kai4ch1 <carl4welps@gmail.com>
2025-09-28 15:13:17 +0300

    "homework 6.2 fix added"

--- lesson_6/homework_6_2.py
+++ lesson_6/homework_6_2.py
@@ -1,12 +1,13 @@
 def task_6_2():
-    user_input = input("Print the word which contains 'h' Or 'H:\n")
-    print(user_input)
-    char_h = "h" or "H"
+    while True:
+        user_input = input("Enter the word which contains 'h' Or 'H:\n")
+        print(user_input)
 
-    if char_h in user_input:
-        print("Congrats your Input contains 'ashes' hu-hu, Just kidding H-s fine :)")
-    else:
-        print("Your input does not contains 'H' or 'h' try again")
+        if "h" in user_input.lower():
+            print("Congrats your Input contains 'ashes' hu-hu, Just kidding H-s fine :)")
+            break
+        else:
+            print("Your input does not contains 'H' or 'h' try again")
 
 if __name__ == "__main__":
     task_6_2()
