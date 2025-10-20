27cdf2948462bc7b5f0476f747b82229fbd9c726
Kai4ch1 <carl4welps@gmail.com>
2025-10-20 12:02:54 +0300

    "homework 11 fix added"

--- lesson_11/homework_11.py
+++ lesson_11/homework_11.py
@@ -1,24 +1,20 @@
 class Logic:
     @staticmethod
-    def parse_the_list(list_1: list[str]):
-        for x in list_1:
-            try:
-                parsed_list = x.split(",")
-                num = [int(num.strip()) for num in parsed_list if num.strip()]
-                result = sum(num)
-                print(result)
-            except ValueError:
-                print("I can`t do it")
+    def sym_number_in_str(str_of_num: str):
+        try:
+            numbers = str_of_num.split(",")
+            result = sum([int(num.strip()) for num in numbers])
+            print(result)
+            return result
+        except ValueError:
+            print("I can`t do it")
 
-
-def main():
+if __name__ == "__main__":
     list_1 = (["1,2,3,4",
                     "1,2,3,4,50",
                     "qwerty1,2,3",
                     "5,5,5,5",
                     "7,7,7,,,7,",
                     "32, 32, 32"])
-
-    Logic.parse_the_list(list_1)
-if __name__ == "__main__":
-    main()
+    for _str in list_1:
+        Logic.sym_number_in_str(_str)
