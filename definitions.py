0fb403aefdfb775afc815790c0eddc99c9c43bf9
macbookair <carl4welps@gmail.com>
2025-11-10 03:19:47 +0200

    HW 16 part 1

--- definitions.py
+++ definitions.py
@@ -8,6 +8,6 @@ BASE_PATH = DEFINITION_FILE.parent
 
 LINK_TO_CSV_FILES = BASE_PATH / "lesson_16" / "1_csv" / "csv_files_to_be_processed"
 
+PROCESSED_CSV_FILE = BASE_PATH / "lesson_16" / "1_csv" / "result_of_2_processed_csv_files.csv"
+
 PATH_TO_CSV_MERGED_FILE = BASE_PATH / "csv_processed_file"
-print(BASE_PATH)
-print(LINK_TO_CSV_FILES)
