e6aa8888077c194bee3fa1d270a452d05933c16b
Kai4ch1 <carl4welps@gmail.com>
2025-11-20 23:14:31 +0200

    Homework 19 fix

--- lesson_19/API_service.py
+++ lesson_19/API_service.py
@@ -6,9 +6,6 @@ url = "https://images-api.nasa.gov/search?q=mars&media_type=image"
 def random_photo(a, b) -> int:
     return random.randint(a, b)
 
-def random_collection(a, b) -> int:
-    return random.randint(a, b)
-
 def api_call():
     response = requests.get(url)
     response_json = response.json()
@@ -16,23 +13,22 @@ def api_call():
 
     return data
 
-def save_random_photo(data, file_name : str, count):
-    count += 1
-    file_name += f"{count}.jpg"
+def save_random_photo(data, file_name : str):
+    file_name += ".jpg"
 
-    with open(file_name, "wb") as ph:
-        try:
-            href = data[random_collection(0, 5)]["href"]
-            nested_links = requests.get(url=href).json()
-            photo = nested_links[random_photo(1, 4)]
-            response = requests.get(url=photo)
-            content = response.content
-            ph.write(bytes(content))
-        except json.JSONDecodeError:
+    try:
+        href = data[random_photo(0, 5)]["href"]
+        nested_links = requests.get(url=href).json()
+        photo = nested_links[random_photo(1, 4)]
+        response = requests.get(url=photo)
+        content = response.content
+        with open(file_name, "wb") as ph:
+                ph.write(bytes(content))
+    except json.JSONDecodeError:
             print("Wrong photo format, upload the new ones and try again")
 
 
 if __name__ == "__main__":
     nasa_files = api_call()
-    save_random_photo(nasa_files, "photo_", 0)
-    save_random_photo(nasa_files, "photo_", 1)
+    save_random_photo(nasa_files, "photo_1")
+    save_random_photo(nasa_files, "photo_2")
